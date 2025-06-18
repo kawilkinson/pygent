7780af6d2f4b6026b5f9b6f6bc06d765bca79a35
kawilkinson <kyle.wilkinson.tech@gmail.com>
2025-06-17 20:44:52 -0400

    create system prompt for ai and give it access to get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory=None) -> str:
     permitted_wd = os.path.abspath(working_directory)
@@ -25,3 +26,18 @@ def get_files_info(working_directory, directory=None) -> str:
         return "\n".join(formatted_directory_contents)
     except Exception as ex:
         return f"Error listing files: {ex}"
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            )
+        }
+    )
+)
--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from system_prompt import system_prompt
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    client = genai.Client(api_key=api_key)
-
     if len(sys.argv) > 1:
         if sys.argv[-1] == "--verbose":
             is_verbose = True
@@ -21,23 +19,43 @@ def main():
         print("No prompt provided, exiting the program")
         sys.exit(1)
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
 
+    if is_verbose:
+        print(f"User Prompt: {prompt}")
+
+    generate_ai_content(client, messages, is_verbose)
+
+
+def generate_ai_content(client, messages, is_verbose):
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt),
     )
 
-
     if is_verbose:
-        print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print("No --verbose flag detected")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+
+    if not response.function_calls:
+        return response.text
+    
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 if __name__=="__main__":
     main()
